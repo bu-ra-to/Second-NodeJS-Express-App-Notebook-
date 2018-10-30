24b3cc542ff3556d022ab1e0a144c7bd95074077
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-10-30 16:01:32 -0400

    fixed auth with redirect

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 
 router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-        res.send('Hello redirect');
+        res.redirect('/dashboard');
     });
 
 module.exports = router;
