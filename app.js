55c57d462646dcff3e2f854438f6e8a5796dd97c
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-11-06 08:40:48 -0500

    google auth complete

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const passport = require('passport')
+const passport = require('passport');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+
+// LOad User Model
+require('./models/Users')
 
 // Passport Config
 require('./config/passport')(passport);
@@ -9,14 +15,43 @@ require('./config/passport')(passport);
 // Loading Routes
 const auth = require('./routes/auth');
 
+// Loading Keys
+const keys = require('./config/keys');
+
+/// Mongo connect
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log('mongo connected'))
+    .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
     res.send("Hello Again Ivan")
 });
 
+//Express-session middleware
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
 
+//Cookie-parser middleware
+app.use(cookieParser())
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+/// Set Global Vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null
+    next()
+})
+
+
+// Use Routes
 app.use('/auth', auth);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport')
+const passport = require('passport');
+
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
@@ -8,7 +9,17 @@ router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
         res.redirect('/dashboard');
     });
-
-module.exports = router;
+router.get('/verify', (req, res) => {
+    if (req.user) {
+        console.log(req.user)
+    } else {
+        console.log("Not Authorized")
+    }
+})
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+})
 
 module.exports = router
+
