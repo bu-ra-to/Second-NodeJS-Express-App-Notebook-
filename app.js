b7c1fdb811b19a4973dbded6ab9b379a3b32a0a6
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-11-15 08:23:32 -0500

    node_modules removed

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const app = express();
 const passport = require('passport');
 const session = require("express-session");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 
-// LOad User Model
-require('./models/Users')
+// LOad Models
+require('./models/Users');
+require('./models/Stories')
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Loading Routes
-const index = require('./routes/index')
+const index = require('./routes/index');
+const stories = require('./routes/stories');
 const auth = require('./routes/auth');
 
 // Loading Keys
@@ -31,8 +35,18 @@ app.use(session({
     saveUninitialized: false
 }))
 
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Handlebars Helpers
+const { truncate, stripTags, formatDate } = require('./helpers/hbs')
+
 // Handlebars middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+    helpers: { truncate: truncate, stripTags: stripTags, formatDate: formatDate },
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 //Cookie-parser middleware
@@ -48,10 +62,14 @@ app.use((req, res, next) => {
     next()
 })
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // Use Routes
 app.use('/', index);
-app.use('/dashboard', index);
+app.use('/stories', stories);
+// app.use('/dashboard', index);
 app.use('/auth', auth);
 
 
